5b6cb4a98d2ebd495239af10503ea29baea911fb
Adrian Gomez <aegomez@tekmexico.com>
2021-07-19 18:10:49 -0700

    Add data fetching for list of launches

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,8 +1,25 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import {
+  ApolloClient,
+  NormalizedCacheObject,
+  ApolloProvider,
+} from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { cache } from './cache';
+import Pages from './pages';
+import injectStyles from './styles';
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   uri: 'http://localhost:4000/graphql',
 });
+
+injectStyles();
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
